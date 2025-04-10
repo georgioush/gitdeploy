a2e82d95d4da003079b16f050f1a819c743a0952
Akira Shoji <georgioush@gmail.com>
2025-04-10 13:37:28 +0900

    sample function

--- function_app.py
+++ function_app.py
@@ -1,6 +1,10 @@
 import azure.functions as func
-import datetime
-import json
 import logging
 
 app = func.FunctionApp()
+
+@app.function_name(name="PingFunction")
+@app.route(route="ping", auth_level=func.AuthLevel.ANONYMOUS)
+def ping(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Ping endpoint was hit.")
+    return func.HttpResponse("pong", status_code=200)
